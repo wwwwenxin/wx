d53352a08eb8b149adca685dd195ea34d7d84858
wwwwenxin <578355367@qq.com>
2015-11-18 21:06:25 +0800

    按钮控制的led延时开关
    当按下按钮1秒钟，灯会亮，然后持续5秒，熄灭

--- sketch_nov18a/sketch_nov18a.ino
+++ sketch_nov18a/sketch_nov18a.ino
@@ -1,15 +1,15 @@
 void setup() 
 {
    // put your setup code here, to run once:
-  pinMode(4,INPUT);
+  pinMode(4,INPUT);     //将4号数字口设置为输入状态,13号设为输出状态
   pinMode(13,OUTPUT);
 
 }
 
 void loop()
 {
-  int n  =digitalRead(4);
-  if (n==HIGH)
+  int n  =digitalRead(4);  //创建一个变量n,将4号数字口的状态采集出来赋值给他
+  if (n==HIGH)    //判断n是否为高电平,如果是执行下面的语句,不是则跳过
   {
 
     delay(1000);
